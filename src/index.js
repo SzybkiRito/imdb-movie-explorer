85f9f1361f661944092929a7823469582ed491f2
SzybkiRito <jakubczakmateusz23@gmail.com>
2023-12-15 17:45:46 +0100

    Convert project to TS

--- src/index.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import "./index.css";
-import App from "./components/app/App.js";
-import reportWebVitals from "./reportWebVitals";
-
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<App />);
-reportWebVitals();
